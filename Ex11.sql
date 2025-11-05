2ab0894e757fb6623381127fb702170a04a34dd2
Lucas <lcorte110210@gmail.com>
2025-11-05 16:11:54 -0300

    Exercício 11 - Itens 1 a 5

--- Ex11.sql
+++ Ex11.sql
@@ -97,3 +97,73 @@ CREATE TABLE tbNotaFiscal (
     dataEmissao DATE NOT NULL
 );
 ALTER TABLE tbVenda ADD FOREIGN KEY (nf) REFERENCES tbNotaFiscal(nf);
+DESCRIBE tbFornecedor;
+INSERT INTO tbFornecedor (cnpj, nome, telefone)
+VALUES 
+	(1245678937123, "Revenda Chico Loco", 11934567897),
+	(1345678937123, "José Faz Tudo S/A", 11934567898),
+    (1445678937123, "Vadalto Entregas", 11934567899),
+    (1545678937123, "Astrogildo das Estrela", 11934567800),
+    (1645678937123, "Amoroso e Doce", 11934567801),
+    (1745678937123, "Marcelo Dedal", 11934567802),
+    (1845678937123, "Franciscano Cachaça", 11934567803),
+    (1945678937123, "Joãozinho Chupeta", 11934567804);
+    
+DESCRIBE tbCidade;
+DELIMITER $$
+CREATE PROCEDURE inserirCidades()
+BEGIN
+    INSERT INTO tbCidade (cidade) VALUES 
+        ('Rio de Janeiro'),
+        ('São Carlos'),
+        ('Campinas'),
+        ('Franco da Rocha'),
+        ('Osasco'),
+        ('Pirituba'),
+        ('Lapa'),
+        ('Ponta Grossa');
+END $$
+DELIMITER ;
+CALL inserirCidades();
+
+DESCRIBE tbEstado;
+DELIMITER $$
+CREATE PROCEDURE inserirEstados()
+BEGIN
+	INSERT INTO tbEstado (uf) VALUES
+		("SP"),
+		("RJ"),
+		("RS");
+END $$
+DELIMITER ;
+CALL inserirEstados();
+
+DESCRIBE tbBairro;
+DELIMITER $$
+CREATE PROCEDURE inserirBairros()
+BEGIN
+	INSERT INTO tbBairro (bairro) VALUES
+		("Aclimação"),
+		("Capão Redondo"),
+		("Pirituba"),
+        ("Liberdade");
+END $$
+DELIMITER ;
+CALL inserirBairros();
+
+DESCRIBE tbProduto;
+DELIMITER $$
+CREATE PROCEDURE inserirProdutos()
+BEGIN
+	INSERT INTO tbProduto (codBarras, nome, valor, qtd) VALUES
+		(12345678910111, "Rei de Papel Mache", 54.61, 120),
+        (12345678910112, "Bolinha de Sabão", 100.45, 120),
+        (12345678910113, "Carro Bate", 44.00, 120),
+        (12345678910114, "Bola Furada", 10.00, 120),
+        (12345678910115, "Maçã Laranja", 99.44, 120),
+        (12345678910116, "Boneco do Hitler", 124.00, 200),
+        (12345678910117, "Farinha de Suruí", 50.00, 200),
+        (12345678910118, "Zelador de Cemitério", 24.50, 100);
+END $$
+DELIMITER ;
+CALL inserirProdutos();
