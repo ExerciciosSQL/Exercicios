55429d70fb261c7183472b7f4b6c2988925cef7a
Lucas <lcorte110210@gmail.com>
2025-11-06 19:21:01 -0300

    Exercício 11 - Itens 6 a 10

--- Ex11.sql
+++ Ex11.sql
@@ -10,7 +10,7 @@ CREATE TABLE tbCliente (
 CREATE TABLE tbClientePF (
 	cpf NUMERIC(11) PRIMARY KEY,
     rg NUMERIC(9) NOT NULL,
-    rgDig NUMERIC(1) NOT NULL,
+    rgDig CHAR(1) NOT NULL,
     nasc DATE NOT NULL,
     id INT NOT NULL,
     FOREIGN KEY (id) REFERENCES tbCliente(id)
@@ -63,7 +63,8 @@ CREATE TABLE tbCompra (
     dataCompra DATE NOT NULL,
     valorTotal DECIMAL(7, 2) NOT NULL,
     qtdTotal SMALLINT NOT NULL,
-    codigo INT NOT NULL
+    codigo INT NOT NULL,
+    FOREIGN KEY (codigo) REFERENCES tbFornecedor(codigo)
 );
 CREATE TABLE tbItemCompra (
 	qtd INT NOT NULL,
@@ -97,6 +98,7 @@ CREATE TABLE tbNotaFiscal (
     dataEmissao DATE NOT NULL
 );
 ALTER TABLE tbVenda ADD FOREIGN KEY (nf) REFERENCES tbNotaFiscal(nf);
+
 DESCRIBE tbFornecedor;
 INSERT INTO tbFornecedor (cnpj, nome, telefone)
 VALUES 
@@ -167,3 +169,126 @@ BEGIN
 END $$
 DELIMITER ;
 CALL inserirProdutos();
+
+DESCRIBE tbEndereco;
+SELECT * FROM tbBairro;
+SELECT * FROM tbCidade;
+SELECT * FROM tbEstado;
+INSERT INTO tbBairro (bairro) VALUES
+	("Lapa"),
+    ("Consolação"),
+    ("Penha"),
+    ("Barra Funda");
+INSERT INTO tbCidade (cidade) VALUES
+	("São Paulo"),
+    ("Barra Mansa");
+DELIMITER $$
+CREATE PROCEDURE inserirEnderecos()
+BEGIN
+	INSERT INTO tbEndereco (cep, logradouro, bairroId, cidadeId, ufId) VALUES
+		(12345050, "Rua da Federal", 5, 9, 1),
+        (12345051, "Av. Brasil", 5, 3, 1),
+        (12345052, "Rua Liberdade", 6, 9, 1),
+        (12345053, "Av. Paulista", 7, 1, 2),
+        (12345054, "Rua Ximbú", 7, 1, 2),
+        (12345055, "Rua Piu XI", 7, 3, 1),
+        (12345056, "Rua Chocolate", 1, 10, 2),
+        (12345057, "Rua Pão na Chapa", 8, 8, 3);
+END $$
+DELIMITER ;
+CALL inserirEnderecos();
+
+DESCRIBE tbCliente;
+DESCRIBE tbClientePF;
+SELECT * FROM tbEndereco;
+INSERT INTO tbBairro (bairro) VALUE ("Jardim Santa Isabel");
+INSERT INTO tbCidade (cidade) VALUE ("Cuiabá");
+INSERT INTO tbEstado (uf) VALUE ("MT");
+INSERT INTO tbEndereco (cep, logradouro, bairroId, cidadeId, ufId) VALUES
+	(12345058, "Rua Veia", 9, 9, 4),
+	(12345059, "Av Nova", 9, 9, 4);
+DELIMITER $$
+CREATE PROCEDURE inserirClientesPF()
+BEGIN
+	INSERT INTO tbCliente (nomeCli, numEnd, compEnd, cepCli) VALUES
+		("Pimpão", 325, Null, 12345051),
+        ("Disney Chaplin", 89, "Ap. 12", 12345053),
+        ("Marciano", 744, Null, 12345054),
+        ("Lança Perfume", 128, Null, 12345059),
+        ("Remédio Amargo", 2585, Null, 12345058);
+	INSERT INTO tbClientePF (cpf, rg, rgDig, nasc, id) VALUES
+		(12345678911, 12345678, "0", "2000-10-12", 1),
+        (12345678912, 12345679, "0", "2001-11-21", 2),
+        (12345678913, 12345680, "0", "2001-06-01", 3),
+        (12345678914, 12345681, "X", "2004-04-05", 4),
+        (12345678915, 12345682, "0", "2002-07-15", 5);
+END $$
+DELIMITER ;
+CALL inserirClientesPF();
+
+DESCRIBE tbCliente;
+DESCRIBE tbClientePJ;
+SELECT * FROM tbEndereco;
+INSERT INTO tbBairro (bairro) VALUE ("Sei Lá");
+INSERT INTO tbCidade (cidade) VALUE ("Recife");
+INSERT INTO tbEstado (uf) VALUE ("PE");
+INSERT INTO tbEndereco (cep, logradouro, bairroId, cidadeId, ufId) VALUE (12345060, "Rua dos Amores", 10, 12, 5);
+DELIMITER $$
+CREATE PROCEDURE inserirClientesPJ()
+BEGIN
+	INSERT INTO tbCliente (nomeCli, numEnd, compEnd, cepCli) VALUES
+		("Paganada", 159, Null, 12345051),
+        ("Disney Chaplin", 69, Null, 12345053),
+        ("Marciano", 189, Null, 12345060),
+        ("Lança Perfume", 5024, "Sala 23", 12345060),
+        ("Remédio Amargo", 1254, Null, 12345060);
+	INSERT INTO tbClientePJ (cnpj, ie, id) VALUES
+		(12345678912345, 98765432198, 6),
+        (12345678912346, 98765432199, 7),
+        (12345678912347, 98765432100, 8),
+        (12345678912348, 98765432101, 9),
+        (12345678912349, 98765432102, 10);
+END $$
+DELIMITER ;
+CALL inserirClientesPJ();
+
+DESCRIBE tbCompra;
+DESCRIBE tbItemCompra;
+SELECT * FROM tbFornecedor;
+SELECT * FROM tbProduto;
+DELIMITER $$
+CREATE PROCEDURE inserirCompras()
+BEGIN
+	INSERT INTO tbCompra (notaFiscal, dataCompra, valorTotal, qtdTotal, codigo) VALUES
+		(8459, STR_TO_DATE("01/05/2018", "%d/%m/%Y"), 21944.00, 700, 5),
+		(2482, STR_TO_DATE("22/04/2020", "%d/%m/%Y"), 7290.00, 180, 1),
+		(21563, STR_TO_DATE("12/07/2020", "%d/%m/%Y"), 900.00, 300, 6),
+		(156354, STR_TO_DATE("23/11/2021", "%d/%m/%Y"), 18900.00, 350, 1);
+	INSERT INTO tbItemCompra (qtd, valorItem, notaFiscal, codBarras) VALUES
+		(200, 22.22, 8459, 12345678910111),
+        (180, 40.50, 2482, 12345678910112),
+        (300, 3.00, 21563, 12345678910113),
+        (500, 35.00, 8459, 12345678910114),
+        (350, 54.00, 156354, 12345678910115);
+END $$
+DELIMITER ;
+CALL inserirCompras();
+
+DESCRIBE tbVenda;
+DESCRIBE tbItemVenda;
+SELECT * FROM tbCliente;
+SELECT * FROM tbProduto;
+DELIMITER $$
+CREATE PROCEDURE inserirVendas()
+BEGIN
+	INSERT INTO tbVenda (numeroVenda, dataVenda, totalVenda, idCli) VALUES
+		(1, NOW(), 54.61, 1),
+        (2, NOW(), 200.90, 4),
+        (3, NOW(), 44.00, 1);
+	INSERT INTO tbItemVenda (numeroVenda, codBarras, valorItem, qtd) VALUES
+		(1, 12345678910111, 54.61, 1),
+        (2, 12345678910112, 100.45, 2),
+        (3, 12345678910113, 44.00, 1);
+END $$
+DELIMITER ;
+CALL inserirVendas();
